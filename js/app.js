e730ee8ce6df31f6238b68a160c2f9c40efe0bc5
a-hr-nikolov <a.hr.nikolov@gmail.com>
2023-04-05 09:20:08 +0300

    Create gameBoard object and move code there

--- js/app.js
+++ js/app.js
@@ -1,14 +1,20 @@
 const gameContainer = document.querySelector('.game-container');
-const gameBoard = {
-  gridCells: [],
-  gridSize: '3x3',
-  getGridSize() {
-    return +gameBoard.gridSize.match(/^\d/);
-  },
-};
+const gameBoard = (boardSizeString => {
+  let boardSize = boardSizeString;
+  let boardCells = [];
+  function getBoardSize() {
+    return +boardSize.match(/^\d/);
+  }
 
-for (let i = 0; i < gameBoard.getGridSize() ** 2; i++) {
-  gameBoard.gridCells[i] = document.createElement('div');
-  gameBoard.gridCells[i].classList.add('cell');
-  gameContainer.appendChild(gameBoard.gridCells[i]);
-}
+  function createBoard() {
+    for (let i = 0; i < getBoardSize() ** 2; i++) {
+      boardCells[i] = document.createElement('div');
+      boardCells[i].classList.add('cell');
+      gameContainer.appendChild(boardCells[i]);
+    }
+  }
+
+  return { createBoard };
+})('3x3');
+
+gameBoard.createBoard();

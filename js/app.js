70e4d08f9e760b3a941ee26ce850392af15fe978
a-hr-nikolov <a.hr.nikolov@gmail.com>
2023-04-05 09:22:51 +0300

    Create a place symbol function that works

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@ const gameContainer = document.querySelector('.game-container');
 const gameBoard = (boardSizeString => {
   let boardSize = boardSizeString;
   let boardCells = [];
+
   function getBoardSize() {
     return +boardSize.match(/^\d/);
   }
@@ -10,8 +11,13 @@ const gameBoard = (boardSizeString => {
     for (let i = 0; i < getBoardSize() ** 2; i++) {
       boardCells[i] = document.createElement('div');
       boardCells[i].classList.add('cell');
+      boardCells[i].addEventListener('click', placeSymbol);
       gameContainer.appendChild(boardCells[i]);
     }
+  }
+
+  function placeSymbol(event) {
+    event.target.textContent = 'x';
   }
 
   return { createBoard };

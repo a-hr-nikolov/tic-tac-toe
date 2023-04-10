f2f3ee29b6713a14c19c3b261bf320a682354945
a-hr-nikolov <a.hr.nikolov@gmail.com>
2023-04-10 16:00:44 +0300

    Remove unnecessary TODO comments

--- js/app.js
+++ js/app.js
@@ -238,10 +238,6 @@ const playerTwo = createPlayer('Player Two', 'O');
 
 // Include grid size adjuster
 
-// Have a Game Controller to handle the Logic, and Display Controller to display without happens
-// The GameBoard object should only have an array of elements, it probably shouldn't be used to handle the display
-// of said elements.
-
 /* Optional - If you’re feeling ambitious create an AI so that a player can play against the computer!
 Start by just getting the computer to make a random legal move.
 Once you’ve gotten that, work on making the computer smart. It is possible to create an unbeatable AI using the minimax algorithm (read about it here, some googling will help you out with this one)

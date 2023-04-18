ab578f6cb4c2d1d8824e0ecb0843ea280c28f2e8
a-hr-nikolov <a.hr.nikolov@gmail.com>
2023-04-18 09:50:10 +0300

    Refactor to using a class

--- js/gameBoard.js
+++ js/gameBoard.js
@@ -1,3 +1,32 @@
+/*
+
+Below I have two ways of implementing the creation of a new game board. Initially,
+I was using the factory function, but decided to switch to a class later on. I still
+wanted to keep the factory function though, just to keep it as a history artifact.
+
+*/
+
+export class GameBoard {
+  #boardState;
+  #gridSize;
+  constructor(gridSizeString) {
+    this.#boardState = [];
+    this.#gridSize = gridSizeString;
+  }
+
+  getBoardState() {
+    return [...this.#boardState];
+  }
+
+  getGridSize() {
+    return (+this.#gridSize.match(/^\d/)) ** 2;
+  }
+
+  setBoardCell = (index, marker) => {
+    this.#boardState[index] = marker;
+  };
+}
+
 export const initGameBoard = gridSizeString => {
   let boardState = [];
 
